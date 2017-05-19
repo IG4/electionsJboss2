395ea5458a589ce1ac4b3d80a3dc8eb03b904f76
krak <krak@KRAK-PCPORTABLE.einet.ad.eivd.ch>
2017-05-19 11:51:59 +0200

    Classes Candidat, Electeur et Parti créées

---
+++ elections_wildfly/src/heig/metier/Candidat.java
@@ -0,0 +1,90 @@
+package heig.metier;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Candidat implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue
+	private int id;
+	@Column(length=20)
+	private String nom;
+	@Column(length=20)
+	private String prenom;
+	@Column(length=30)
+	private Date ddn;
+	@Column(length=30)
+	private String localite;
+	@Column(length=30)
+	private String parti;
+
+	
+	public Candidat() {
+		super();
+		id = 0;
+	}
+	
+	public Candidat(int id, String nom, String prenom, Date ddn, String localite, String parti) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.ddn = ddn;
+		this.localite = localite;
+		this.parti = parti;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDdn() {
+		return ddn;
+	}
+
+	public void setDdn(Date ddn) {
+		this.ddn = ddn;
+	}
+
+	public String getLocalite() {
+		return localite;
+	}
+
+	public void setLocalite(String localite) {
+		this.localite = localite;
+	}
+
+	public String getParti() {
+		return parti;
+	}
+
+	public void setParti(String parti) {
+		this.parti = parti;
+	}
+
+	
+}
--- elections_wildfly/src/heig/metier/Electeur.java
+++ elections_wildfly/src/heig/metier/Electeur.java
@@ -1,19 +1,40 @@
 package heig.metier;
 
-public class Electeur {
+import java.io.Serializable;
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Electeur implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+
+@Id @GeneratedValue
 	private int id;
+	@Column(length=20)
 	private String nom;
+	@Column(length=20)
 	private String prenom;
+	@Column(length=30)
+	private Date ddn;
+	@Column(length=30)
+	private String localite;
 
 	public Electeur() {
 		super();
+		id = 0;
 	}
 
-	public Electeur(int id, String nom, String prenom) {
+	public Electeur(int id, String nom, String prenom, Date ddn, String localite) {
 		this.id = id;
 		this.nom = nom;
 		this.prenom = prenom;
+		this.ddn = ddn;
+		this.localite = localite;
 	}
 
 	public int getId() {
@@ -38,6 +59,22 @@ public class Electeur {
 
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
+	}
+
+	public Date getDdn() {
+		return ddn;
+	}
+
+	public void setDdn(Date ddn) {
+		this.ddn = ddn;
+	}
+
+	public String getLocalite() {
+		return localite;
+	}
+
+	public void setLocalite(String localite) {
+		this.localite = localite;
 	}
 
 	@Override
